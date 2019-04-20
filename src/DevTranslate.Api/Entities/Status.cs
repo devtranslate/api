09b88951e0d5a4c8df4f7a718181e891bbe238e2
rodrigolira <rvlira83@gmail.com>
2019-04-20 20:35:05 -0300

    :construction: feat: modeling the Translation type

---
+++ src/DevTranslate.Api/Entities/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTranslate.Api.Entities
+{
+    public enum Status
+    {
+        Completed = 1,
+        InProgress
+    }
+}
