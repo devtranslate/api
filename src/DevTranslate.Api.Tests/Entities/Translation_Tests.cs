5168316feb490d212224109c6cff3a32206e1b45
rodrigolira <rvlira83@gmail.com>
2019-04-20 20:36:13 -0300

    :white_check_mark: test: adding tests of the Translation type

---
+++ src/DevTranslate.Api.Tests/Entities/Translation_Tests.cs
@@ -0,0 +1,37 @@
+﻿using DevTranslate.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DevTranslate.Api.Tests.Entities
+{
+    public class Translation_Tests
+    {
+        [Fact]
+        public void Should_CreateInstance_When_Instantiating()
+        {
+            var translation = new Translation(
+                title: "Pare de usar CSS no JavaScript para desenvolvimento web",
+                author: null,
+                translator: null,
+                language: Language.English,
+                url: null,
+                imageUrl: null,
+                status: Status.Completed,
+                type: TranslationType.Articles
+            );
+
+            Assert.NotNull(translation);
+        }
+
+        [Fact]
+        public void Should_ThrowException_When_TitleIsNullOrEmpty()
+        {
+            Assert.Throws<ArgumentNullException>("title", () =>
+            {
+                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+            });
+        }
+    }
+}
