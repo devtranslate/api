7e52451a561a8b05c1f865e834d4fbe2c8df1ca8
rodrigolira <rvlira83@gmail.com>
2019-06-30 22:52:10 -0300

    :white_check_mark: test: updating unit tests

--- src/DevTranslate.Api.Tests/Entities/Translation_Tests.cs
+++ src/DevTranslate.Api.Tests/Entities/Translation_Tests.cs
@@ -25,48 +25,103 @@ namespace DevTranslate.Api.Tests.Entities
             Assert.NotNull(translation);
         }
 
-        [Fact]
-        public void Should_ThrowException_When_TitleIsNullOrEmpty()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_ThrowException_When_TitleIsNullOrEmpty(string titulo)
         {
             Assert.Throws<ArgumentNullException>("title", () =>
             {
-                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+                var translation = new Translation(
+                    title: titulo,
+                    author: "Gajus Kuizinas",
+                    translator: "Carlos Eduardo Olivieri",
+                    language: Language.English,
+                    url: "https://medium.com/@cadutech/pare-de-usar-css-no-javascript-para-desenvolvimento-web-b4a2e8fbb85e",
+                    imageUrl: "https://image.ibb.co/b9GdKb/1.png",
+                    status: Status.Completed,
+                    type: TranslationType.Articles
+                );
             });
         }
 
-        [Fact]
-        public void Should_ThrowException_When_AuthorIsNullOrEmpty()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_ThrowException_When_AuthorIsNullOrEmpty(string author)
         {
             Assert.Throws<ArgumentNullException>("author", () =>
             {
-                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+                var translation = new Translation(
+                    title: "Pare de usar CSS no JavaScript para desenvolvimento web",
+                    author: author,
+                    translator: "Carlos Eduardo Olivieri",
+                    language: Language.English,
+                    url: "https://medium.com/@cadutech/pare-de-usar-css-no-javascript-para-desenvolvimento-web-b4a2e8fbb85e",
+                    imageUrl: "https://image.ibb.co/b9GdKb/1.png",
+                    status: Status.Completed,
+                    type: TranslationType.Articles
+                );
             });
         }
 
-        [Fact]
-        public void Should_ThrowException_When_TranslatorIsNullOrEmpty()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_ThrowException_When_TranslatorIsNullOrEmpty(string translator)
         {
             Assert.Throws<ArgumentNullException>("translator", () =>
             {
-                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+                var translation = new Translation(
+                    title: "Pare de usar CSS no JavaScript para desenvolvimento web",
+                    author: "Gajus Kuizinas",
+                    translator: translator,
+                    language: Language.English,
+                    url: "https://medium.com/@cadutech/pare-de-usar-css-no-javascript-para-desenvolvimento-web-b4a2e8fbb85e",
+                    imageUrl: "https://image.ibb.co/b9GdKb/1.png",
+                    status: Status.Completed,
+                    type: TranslationType.Articles
+                );
             });
         }
 
-        [Fact]
-        public void Should_ThrowException_When_UrlIsNullOrEmpty()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_ThrowException_When_UrlIsNullOrEmpty(string url)
         {
             Assert.Throws<ArgumentNullException>("url", () =>
             {
-                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+                var translation = new Translation(
+                    title: "Pare de usar CSS no JavaScript para desenvolvimento web",
+                    author: "Gajus Kuizinas",
+                    translator: "Carlos Eduardo Olivieri",
+                    language: Language.English,
+                    url: url,
+                    imageUrl: "https://image.ibb.co/b9GdKb/1.png",
+                    status: Status.Completed,
+                    type: TranslationType.Articles
+                );
             });
         }
 
-        [Fact]
-        public void Should_ThrowException_When_ImageUrlIsNullOrEmpty()
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void Should_ThrowException_When_ImageUrlIsNullOrEmpty(string imageUrl)
         {
             Assert.Throws<ArgumentNullException>("imageUrl", () =>
             {
-                var translation = new Translation(null, null, null, Language.English, null, null, Status.Completed, Api.Entities.TranslationType.Articles);
+                var translation = new Translation(
+                    title: "Pare de usar CSS no JavaScript para desenvolvimento web",
+                    author: "Gajus Kuizinas",
+                    translator: "Carlos Eduardo Olivieri",
+                    language: Language.English,
+                    url: "https://medium.com/@cadutech/pare-de-usar-css-no-javascript-para-desenvolvimento-web-b4a2e8fbb85e",
+                    imageUrl: imageUrl,
+                    status: Status.Completed,
+                    type: TranslationType.Articles
+                );
             });
         }
     }
