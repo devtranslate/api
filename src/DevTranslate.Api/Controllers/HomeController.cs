48812998f8cc51e7031804f1039e3aaf1fc34f4c
rodrigolira <rvlira83@gmail.com>
2019-07-06 21:10:31 -0300

    :wrench: chore: updating the launch settings

---
+++ src/DevTranslate.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTranslate.Api.Controllers
+{
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult GetHome()
+        {
+            return Ok();
+        }
+    }
+}
