083cf9c757b35bf2b23d98a771cc44875216d129
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2020-10-25 22:04:08 -0300

    :wrench: feat: adding support for a reverse proxy

--- src/DevTranslate.Api/Startup.cs
+++ src/DevTranslate.Api/Startup.cs
@@ -2,6 +2,7 @@
 using DevTranslate.Api.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,28 +13,49 @@ namespace DevTranslate.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
 
+            
+
             services.AddDbContext<DevTranslateContext>(options =>
             {
                 options.UseMySql(Configuration.GetConnectionString("DevTranslateConnectionString"), mySqlOptions =>
                     {
-                        mySqlOptions.ServerVersion(new Version(5, 7), ServerType.MySql);
+                        if (Environment.IsDevelopment())
+                        {
+                            mySqlOptions.ServerVersion(new Version(5, 7), ServerType.MySql);
+                        }
+
+                        if (Environment.IsProduction())
+                        {
+                            mySqlOptions.ServerVersion(new Version(8, 0), ServerType.MySql);
+                        }
                     });
             });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsProduction())
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
