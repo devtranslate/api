4f90365632ba4a28e450e74531c42d9e42c93ba2
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2022-03-19 17:52:22 -0300

    :sparkles: feat: adding total number of records to the pagination response object

--- src/DevTranslate.Api/DTO/PaginationResponse.cs
+++ src/DevTranslate.Api/DTO/PaginationResponse.cs
@@ -1,20 +1,14 @@
 ﻿using DevTranslate.Api.Shared;
 using Swashbuckle.AspNetCore.Annotations;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DevTranslate.Api.DTO
 {
     public class PaginationResponse
     {
-        private readonly int _totalRecords;
-
         public PaginationResponse(Pagination pagination, int totalRecords)
         {
-            _totalRecords = totalRecords;
-
+            TotalRecords = totalRecords;
             Page = pagination.Page;
             PageSize = pagination.PageSize;
             TotalPages = Convert.ToInt32(Math.Ceiling(totalRecords / Convert.ToDecimal(pagination.PageSize)));
@@ -28,5 +22,8 @@ namespace DevTranslate.Api.DTO
 
         [SwaggerSchema("Total number of page of records available for the requested page size.", ReadOnly = false)]
         public int TotalPages { get; }
+
+        [SwaggerSchema("Total number of records found given the selected filter and pagination settings", ReadOnly = false)]
+        public int TotalRecords { get; }
     }
 }
